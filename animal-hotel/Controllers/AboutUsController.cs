4d800655ce9fef9e7941676369f6b9cab1d1705d
Magdalena Staniec <m.staniec.657@studms.ug.edu.pl>
2021-02-13 00:12:23 +0000

    l

---
+++ animal-hotel/Controllers/AboutUsController.cs
@@ -0,0 +1,22 @@
+﻿using animal_hotel.Models;
+using animal_hotel.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace animal_hotel.Controllers
+{
+    public class AboutUsController : Controller
+    {
+
+        [Route("AboutUs")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+    }
+}
+
